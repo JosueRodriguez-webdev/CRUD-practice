db4b4f0c516c30715c3d1ef981734160f5c85240
Josue Rodriguez <smexyjosue@gmail.com>
2020-02-27 16:40:17 -0800

    Initialize commit - initial commit

--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 export default function Login(props) {
-    // How can we log in? What do we need to do?
+  // How can we log in? What do we need to do?
+  const [userInput, setUserInput] = useState({
+    username: "",
+    password: ""
+  });
 
-    return (
-        <div>
-            <h1>Welcome to the Safari App!</h1>
-            <h2>I can't show you more until you log in. Please build out a login.</h2>
-        </div>
-    )
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post(`/api/login`, userInput)
+      .then((res) => {
+        window.localStorage.setItem("token", res.data.payload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Welcome to the Safari App!</h1>
+      <h2>I can't show you more until you log in. Please build out a login.</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          name="username"
+          label="username"
+          className="input"
+          onChange={handleChange}
+          value={userInput.username}
+        />
+
+        <label htmlFor="password">password</label>
+        <input
+          name="password"
+          label="password"
+          className="input"
+          onChange={handleChange}
+          value={userInput.password}
+        />
+
+        <button className="start" type="submit">
+          Start
+        </button>
+      </form>
+    </div>
+  );
 }
